f50f29a39b973a2507bb9d04023dcba58bb90b94
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-02-22 09:08:26 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import isaac from './isaac.jpg';
 import './App.css';
 
-import  BrowserRouter  from "react-router-dom"
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-
 
 function App() {
     return (
