b412436e05a60cc06bedc65a091562cea677a035
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-03-02 17:31:51 -0500

    added test 3D model to the 3D modeling page

--- src/Modeling.js
+++ src/Modeling.js
@@ -3,10 +3,10 @@ import React from "react"
 const Modeling = () => {
     return (
         <div className="sketchfab-embed-wrapper">
-            <iframe title="A saint with a book" frameBorder="0" allowFullScreen mozallowfullscreen="true"
+            <iframe title="odm_textured_model_geo" frameBorder="0" allowFullScreen mozallowfullscreen="true"
                     webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
                     execution-while-out-of-viewport execution-while-not-rendered web-share
-                    src="https://sketchfab.com/models/2d1a959aa26446219968b5401f853d38/embed"></iframe>
+                    src="https://sketchfab.com/models/4bcc4fc75afa4e35827e3910e035100e/embed"></iframe>
             <p>PAGE IN PROGRESS</p>
         </div>
     );
