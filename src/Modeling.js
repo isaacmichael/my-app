606aed19d3dc5cee5e9e99d886a6a733c5c8a272
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-03-02 17:28:32 -0500

    added test 3D model to the 3D modeling page

--- src/Modeling.js
+++ src/Modeling.js
@@ -7,7 +7,7 @@ const Modeling = () => {
                     webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
                     execution-while-out-of-viewport execution-while-not-rendered web-share
                     src="https://sketchfab.com/models/2d1a959aa26446219968b5401f853d38/embed"></iframe>
-            <p>Hello</p>
+            <p>PAGE IN PROGRESS</p>
         </div>
     );
 }
