3d46cfea2defd07e7475aabdba4cf6a518a77048
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-03-02 17:27:31 -0500

    added test 3D model to the 3D modeling page

--- src/Modeling.js
+++ src/Modeling.js
@@ -1,10 +1,14 @@
-
-
+import React from "react"
 
 const Modeling = () => {
     return (
-        <p>Work in Progress</p>
-
+        <div className="sketchfab-embed-wrapper">
+            <iframe title="A saint with a book" frameBorder="0" allowFullScreen mozallowfullscreen="true"
+                    webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
+                    execution-while-out-of-viewport execution-while-not-rendered web-share
+                    src="https://sketchfab.com/models/2d1a959aa26446219968b5401f853d38/embed"></iframe>
+            <p>Hello</p>
+        </div>
     );
 }
 
