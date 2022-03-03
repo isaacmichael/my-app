4f718b39c5f029721ff04e8f6eaf4c86cb3b8595
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-03-03 13:18:08 -0500

    adjustments to models page

--- src/Modeling.js
+++ src/Modeling.js
@@ -29,40 +29,42 @@ const Modeling = () => {
             </section>
 
             <div className={styles.gallery}>
-                <div className={styles.thumbnail}><a href="#"><img
-                    src="file:///C|/Users/isaac/AppData/Roaming/Adobe/Dreamweaver 2021/en_US/Configuration/Temp/Assets/eam292C.tmp/images/bkg_06.jpg"
-                    alt="" width="2000" className="cards"/></a>
-                    <h4>TITLE</h4>
-                    <p className={styles.tag}>HTML, CSS, JS, WordPress</p>
-                    <p className={styles.text_column}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className={styles.thumbnail}>
+                    <iframe className={styles.threeD} width="500" height="300" src="https://www.youtube.com/embed/5hgq2SEYpa0"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen>
+                    </iframe>
+                    <h4>Best-One Tire Upland, IN</h4>
+                    <p className={styles.tag}>Made with OpenDroneMap</p>
+                    <p className={styles.text_column}>A model of Best-One Tire upland. Taken With a Phantom 4 Pro Drone
+                    Crosshatch survey consisting of ~500 photos for better 3D imaging.
+
+                    </p>
                 </div>
                 <div className={styles.thumbnail}><a href="#"><img
                     src="file:///C|/Users/isaac/AppData/Roaming/Adobe/Dreamweaver 2021/en_US/Configuration/Temp/Assets/eam292C.tmp/images/bkg_06.jpg"
                     alt="" width="2000" className="cards"/></a>
                     <h4>TITLE</h4>
                     <p className={styles.tag}>HTML, CSS, JS, WordPress</p>
-                    <p className={styles.text_column}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p className={styles.text_column}>Pictures of 3D prints of 3D models</p>
                 </div>
 
                 <div className={styles.thumbnail}>
-                    <iframe title="odm_textured_model_geo" frameBorder="0" allowFullScreen mozallowfullscreen="true"
+                    <iframe className={styles.threeD} title="odm_textured_model_geo" frameBorder="0" allowFullScreen mozallowfullscreen="true"
                             webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
                             execution-while-out-of-viewport execution-while-not-rendered web-share
                             src="https://sketchfab.com/models/4bcc4fc75afa4e35827e3910e035100e/embed"></iframe>
-                    <h4>TITLE</h4>
-                    <p className={styles.tag}>HTML, CSS, JS, WordPress</p>
-                    <p className={styles.text_column}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h4>3D Model Of Crops</h4>
+                    <p className={styles.tag}>Processed in Agisoft Metashape</p>
+                    <p className={styles.text_column}>Compiled # photos for this...</p>
                 </div>
                 <div className={styles.thumbnail}><a href="#"><img
                     src="file:///C|/Users/isaac/AppData/Roaming/Adobe/Dreamweaver 2021/en_US/Configuration/Temp/Assets/eam292C.tmp/images/bkg_06.jpg"
                     alt="" width="2000" className={styles.cards}/></a>
-                    <h4>TITLE</h4>
+                    <h4>Shapeoko 3XL</h4>
                     <p className={styles.tag}>HTML, CSS, JS, WordPress</p>
-                    <p className={styles.text_column}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p className={styles.text_column}>Show images or video of shapeoko working on something</p>
                 </div>
             </div>
 
