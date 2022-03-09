e1f29cee8a09d7932f720635cdd658ec93b5cdb2
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-03-09 14:19:17 -0500

    adjustments to models page

--- src/Modeling.js
+++ src/Modeling.js
@@ -15,7 +15,7 @@ const Modeling = () => {
 
             <section className={styles.intro}>
                 <div className={styles.column}>
-                    <h3>3D MODELING</h3>
+                    <h3>3D MODELING - IN PROGRESS</h3>
 
                     <div className={styles.column}>
                         <p>Here you can see what i'm up to. I'll post about photogrammetry, fusion 360, orthomosaics, height maps, and even some CNC videos. Exciting stuff! </p>
@@ -35,9 +35,11 @@ const Modeling = () => {
                     <p className={styles.tag}>Made with OpenDroneMap</p>
                     <p className={styles.text_column}>A model of Best-One Tire upland. Taken With a Phantom 4 Pro Drone
                     Crosshatch survey consisting of ~500 photos for better 3D imaging.
+                        I created this map to keep a 3D record of this portion of the town before it undergoes changes.
 
                     </p>
                 </div>
+
                 <div className={styles.thumbnail}><a>
                     <div className="sketchfab-embed-wrapper">
                         <iframe className={styles.threeD} title="Andersontile2020" frameBorder="0" allowFullScreen mozallowfullscreen="true"
@@ -45,12 +47,20 @@ const Modeling = () => {
                                 xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered
                                 web-share
                                 src="https://sketchfab.com/models/9f871c53335b4b10bda56d19f14fb7e8/embed?autospin=1&autostart=1&preload=1"></iframe>
+                        <img src={elevationPic} alt="elevation Picture"  className={styles.picture} />
                     </div>
-                    <div><img src={elevationPic} alt="elevation Picture"  className={styles.picture} /></div>
+
                 </a>
                     <h4>3D model of crops</h4>
                     <p className={styles.tag}>Made in OpenDroneMap</p>
-                    <p className={styles.text_column}>stuff</p>
+                    <p className={styles.text_column}>
+                        Orthomosaic above was made in an open source project known as OpenDroneMap.
+                        This model took about ~700 photos to get important elevation maps and GLI maps.
+
+                        Below the model, using a dense cloud, the image shows the elevation change as it goes over a house and a portion of a field.
+                        This can be useful for planning where to put in drainage tiles.
+
+                    </p>
                 </div>
 
                 <div className={styles.thumbnail}>
@@ -69,8 +79,8 @@ const Modeling = () => {
                 <div className={styles.thumbnail}><a href="#"><img
                     src="file:///C|/Users/isaac/AppData/Roaming/Adobe/Dreamweaver 2021/en_US/Configuration/Temp/Assets/eam292C.tmp/images/bkg_06.jpg"
                     alt="" width="2000" className={styles.cards}/></a>
-                    <h4>Shapeoko 3XL</h4>
-                    <p className={styles.tag}>HTML, CSS, JS, WordPress</p>
+                    <h4>Shapeoko 3XL - CNC</h4>
+                    <p className={styles.tag}>TO BE ADDED</p>
                     <p className={styles.text_column}>Show images or video of shapeoko working on something</p>
                 </div>
             </div>
