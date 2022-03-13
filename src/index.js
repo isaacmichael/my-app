fbc0db20328deeb1541f0756b20d7f35f72e0209
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-03-12 19:59:15 -0500

    changed browserRouter to HashRouter to better fit GH pages static site. Additional routing changes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './CssModules/index.css';
 import App from './App';
+import Home from './Home';
+
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom"
+import {BrowserRouter, HashRouter} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <App />
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
