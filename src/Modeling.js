809c8af3b60c1efa0b84e0c1e4ca1e82b8a275fb
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-03-20 23:49:58 -0400

    added more info and CNC to the modeling page

--- src/Modeling.js
+++ src/Modeling.js
@@ -18,7 +18,9 @@ const Modeling = () => {
                     <h3>3D MODELING - IN PROGRESS</h3>
 
                     <div className={styles.column}>
-                        <p>Here you can see what i'm up to. I'll post about photogrammetry, fusion 360, orthomosaics, height maps, and even some CNC videos. Exciting stuff! </p>
+                        <p>Here's where I will share by hobbies. I enjoy creating 3D reconstructions of terrain using a DJI Phantom 4 Pro Drone,
+                            carving intricate designs into wood with my Shapeoko 3 XL, and other random things that I find interesting.
+                        </p>
                         <p></p>
                     </div>
                 </div>
@@ -35,7 +37,8 @@ const Modeling = () => {
                     <p className={styles.tag}>Made with OpenDroneMap</p>
                     <p className={styles.text_column}>A model of Best-One Tire upland. Taken With a Phantom 4 Pro Drone
                     Crosshatch survey consisting of ~500 photos for better 3D imaging.
-                        I created this map to keep a 3D record of this portion of the town before it undergoes changes.
+                        Mainly, I created this map for fun, but I also find it important to preserve the state of a given location with something
+                        more substantial than a photo. If a picture is worth a thousand words, I wonder what a 3D model is worth?
 
                     </p>
                 </div>
@@ -47,6 +50,7 @@ const Modeling = () => {
                                 xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered
                                 web-share
                                 src="https://sketchfab.com/models/9f871c53335b4b10bda56d19f14fb7e8/embed?autospin=1&autostart=1&preload=1"></iframe>
+                       <p className={styles.text_column}> Above is an interactive 3D model, try it out!</p>
                         <img src={elevationPic} alt="elevation Picture"  className={styles.picture} />
                     </div>
 
@@ -58,8 +62,12 @@ const Modeling = () => {
                         This model took about ~700 photos to get important elevation maps and GLI maps.
 
                         Below the model, using a dense cloud, the image shows the elevation change as it goes over a house and a portion of a field.
-                        This can be useful for planning where to put in drainage tiles.
-
+                        This could be useful for planning where to put in drainage tiles. For instance, a farmer could put markers in the field
+                        where they expect the lowest spots are. After, a drone could survey and create a map like the one above. Drawing lines
+                        from one flag to the other will show if there are any spots where a trencher would need to compensate for unwanted changes in height or if
+                        a new line should be plotted.
+                    </p>
+                    <p>
                     </p>
                 </div>
 
@@ -76,12 +84,17 @@ const Modeling = () => {
 
                     <p className={styles.text_column}>Compiled # photos for this...</p>
                 </div>
-                <div className={styles.thumbnail}><a href="#"><img
-                    src="file:///C|/Users/isaac/AppData/Roaming/Adobe/Dreamweaver 2021/en_US/Configuration/Temp/Assets/eam292C.tmp/images/bkg_06.jpg"
-                    alt="" width="2000" className={styles.cards}/></a>
+                <div className={styles.thumbnail}>
+                    <iframe className={styles.threeD} width="500" height="300" src="https://www.youtube.com/embed/Zz_SgvYye8Y"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
                     <h4>Shapeoko 3XL - CNC</h4>
-                    <p className={styles.tag}>TO BE ADDED</p>
-                    <p className={styles.text_column}>Show images or video of shapeoko working on something</p>
+                    <p className={styles.tag}>Last Supper Relief Carving</p>
+                    <p className={styles.text_column}>Here's a sped up version of my Shapeoko carving out a last supper scene.
+                    The .OBJ file for the project was taken from Etsy and the GRBL files were created using Vectric Vcarve.
+                        Total time to produce was around 6hrs with 4 bit changes.
+                    </p>
                 </div>
             </div>
 
